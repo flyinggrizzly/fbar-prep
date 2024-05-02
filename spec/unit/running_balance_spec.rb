9e4deb957b1907432efbdaf251319e8f175ff3c1
Sean DMR <sean@flyinggrizzly.net>
2024-05-02 16:56:26 +0100

    Add missing tests for txn date lookups

--- spec/unit/running_balance_spec.rb
+++ spec/unit/running_balance_spec.rb
@@ -5,6 +5,55 @@ require './lib/account'
 
 RSpec.describe FBARPrep::RunningBalance do
   describe '#highest_balance_date' do
+    describe 'provided a date range' do
+      it 'returns the highest balance within the date range' do
+        out_of_range = Date.new(2020, 07, 21)
+        range_begin = Date.new(2020, 07, 22)
+        range_end = Date.new(2020, 07, 23)
+
+        transactions = [
+          transaction(out_of_range, 10_000.00),
+          transaction(range_begin, 9_000.00),
+          transaction(range_end, 9_087.00),
+        ]
+
+        balance = FBARPrep::RunningBalance.new(transactions)
+
+        [:eod, :max].each do |strategy|
+          expect(balance.highest_balance_date(strategy)).to eq(out_of_range)
+          expect(balance.highest_balance_date(strategy, range_begin..range_end)).to eq(range_end)
+        end
+      end
+
+      it 'returns nil if all transactions post date the range' do
+        txn_date = Date.new(2021, 1, 1)
+        early_range = (txn_date - 2.days)...(txn_date - 1.days)
+
+        transactions = [transaction(txn_date, 1)]
+
+        balance = described_class.new(transactions)
+
+        [:eod, :max].each do |strategy|
+          expect(balance.highest_balance_date(strategy, early_range)).to eq(nil)
+        end
+      end
+
+      it 'returns the range begin date if all transactions precede the date range' do
+        txn_date = Date.new(2021, 1, 1)
+
+        range_begin = txn_date + 1.day
+        later_range = range_begin...(txn_date + 4.days)
+
+        transactions = [transaction(txn_date, 1)]
+
+        balance = described_class.new(transactions)
+
+        [:eod, :max].each do |strategy|
+          expect(balance.highest_balance_date(strategy, later_range)).to eq(range_begin)
+        end
+      end
+    end
+
     context 'strategy is :max' do
       it 'returns the right date' do
         transactions = [
